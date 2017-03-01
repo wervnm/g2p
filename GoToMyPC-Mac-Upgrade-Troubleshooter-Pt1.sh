f1dcbb19fc9a9764c3ce35e7eb3b830939395909
Brian Carter <robotmachine@gmail.com>
2017-03-01 08:43:53 -0500

    Split out Upgrade Troubleshooter

--- GoToMyPC-Mac-Upgrade-Troubleshooter-Pt1.sh
+++ GoToMyPC-Mac-Upgrade-Troubleshooter-Pt1.sh
@@ -2,18 +2,9 @@
 
 # Sets variables for the script.
 tempDir=$HOME/Desktop/.g2pts
-archiveName=$HOME/Desktop/GoToMyPC_MUTS.tgz
 rdScript=$tempDir/G2P_Daemon.sh
 g2pPlist=/Library/Preferences/com.citrixonline.GoToMyPC.plist
 #
-# Cleanup
-cleanup() {
-    rm ~/Desktop/.g2pts/G2P_Before.plist
-    rm ~/Desktop/.g2pts/G2P_After.plist
-    rm ~/Desktop/.g2pts/G2P_Daemon.sh
-    rmdir ~/Desktop/.g2pts
-}
-trap "cleanup" EXIT
 # Create tempDir
 if [[ ! -e $tempDir ]]; then
     mkdir $tempDir
@@ -30,8 +21,5 @@ echo "sudo launchctl start com.citrixonline.GoToMyPC.CommAgent" >> $rdScript
 # Copy .plist, restart daemon, and then copy again.
 cp $g2pPlist $tempDir/G2P_Before.plist
 osascript -e 'do shell script "sudo sh $HOME/Desktop/.g2pts/G2P_Daemon.sh >> $HOME/Library/Logs/com.citrixonline.g2pts.log 2>&1" with administrator privileges'
-osascript -e 'tell app "System Events" to display dialog "Waiting 60 seconds to allow upgrade"'
-sleep 60
-cp $g2pPlist $tempDir/G2P_After.plist
-
-tar -cz --exclude="G2P_Daemon.sh" -f $archiveName -C $tempDir .
+#
+# The Automator script will now pause and wait for further input to collect G2P_After.plist and create the .tgz
--- GoToMyPC-Mac-Upgrade-Troubleshooter-Pt2.sh
+++ GoToMyPC-Mac-Upgrade-Troubleshooter-Pt2.sh
@@ -3,7 +3,6 @@
 # Sets variables for the script.
 tempDir=$HOME/Desktop/.g2pts
 archiveName=$HOME/Desktop/GoToMyPC_MUTS.tgz
-rdScript=$tempDir/G2P_Daemon.sh
 g2pPlist=/Library/Preferences/com.citrixonline.GoToMyPC.plist
 #
 # Cleanup
@@ -14,24 +13,9 @@ cleanup() {
     rmdir ~/Desktop/.g2pts
 }
 trap "cleanup" EXIT
-# Create tempDir
-if [[ ! -e $tempDir ]]; then
-    mkdir $tempDir
-fi
 #
-# Create daemon restart script
-touch "$rdScript"
-echo "#!/bin/bash" >> $rdScript
-echo "" >> $rdScript
-echo "sudo launchctl stop com.citrixonline.GoToMyPC.CommAgent" >> $rdScript
-echo "sleep 5" >> $rdScript
-echo "sudo launchctl start com.citrixonline.GoToMyPC.CommAgent" >> $rdScript
-#
-# Copy .plist, restart daemon, and then copy again.
-cp $g2pPlist $tempDir/G2P_Before.plist
-osascript -e 'do shell script "sudo sh $HOME/Desktop/.g2pts/G2P_Daemon.sh >> $HOME/Library/Logs/com.citrixonline.g2pts.log 2>&1" with administrator privileges'
-osascript -e 'tell app "System Events" to display dialog "Waiting 60 seconds to allow upgrade"'
-sleep 60
+# Copy G2P_After.plist
 cp $g2pPlist $tempDir/G2P_After.plist
-
+#
+# Create .tgz
 tar -cz --exclude="G2P_Daemon.sh" -f $archiveName -C $tempDir .
