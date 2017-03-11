77341a3223ef895beb47f9dbb8a8224b6d63215b
Brian A. Carter <robotmachine@protonmail.ch>
2017-03-11 11:12:26 -0500

    Adding LogMeIn Diagnostic tool

---
+++ LogMeInDiagnostic.bat
@@ -0,0 +1,58 @@
+@echo off
+date /t >> %userprofile%\Desktop\LogMeIn_Diagnostics.txt
+@echo Gathering diagnostic info.  This may take a few minutes.
+@echo[
+@echo off
+@echo ---System Summary--- >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+systeminfo | findstr /r "OS.Name OS.Version System.Name System.Model System.Manufacturer Windows.Directory System.Directory Locale User.Name Time.Zone Serial.Number Mac.address Total.Physical.Memory Available.Physical.Memory" >> %userprofile%\Desktop\LogMeIn_Diagnostics.txt
+@echo[
+@echo System Summary complete.  Now retrieving detailed information . . .
+@echo[
+@echo off
+wmic /APPEND:"%userprofile%\Desktop\LogMeIn_Diagnostics.txt" cpu get Name  >nul 2>nul
+
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo ---Video and Display--- >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+wmic /APPEND: "%userprofile%\Desktop\LogMeIn_Diagnostics.txt" path Win32_videocontroller GET description >nul 2>nul
+wmic /APPEND: "%userprofile%\Desktop\LogMeIn_Diagnostics.txt" path Win32_videocontroller GET InstalledDisplayDrivers,DriverVersion >nul 2>nul
+wmic /APPEND: "%userprofile%\Desktop\LogMeIn_Diagnostics.txt" path Win32_videocontroller GET VideoModeDescription >nul 2>nul
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo ---Logical Drives--- >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+wmic /APPEND: "%userprofile%\Desktop\LogMeIn_Diagnostics.txt" logicaldisk get Name,Description,FreeSpace,VolumeName >nul 2>nul
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo ---Network Interface Card--- >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+wmic /APPEND:"%userprofile%\Desktop\LogMeIn_Diagnostics.txt" nic get AdapterType,Name,MACAddress >nul 2>nul
+wmic /APPEND:"%userprofile%\Desktop\LogMeIn_Diagnostics.txt" nicconfig get IPaddress,DefaultIPGateway,IPSubnet,DNSHostName,DNSDomain >nul 2>nul
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo ---Printers--- >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+wmic /APPEND: "%userprofile%\Desktop\LogMeIn_Diagnostics.txt" printer get Name,DriverName,PortName,PrintProcessor,PrintJobDataType >nul 2>nul
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo ---Running Processes--- >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+wmic /APPEND: "%userprofile%\Desktop\LogMeIn_Diagnostics.txt" process get Name,ExecutablePath >nul 2>nul
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo ---Installed Applications--- >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+wmic /APPEND:"%userprofile%\Desktop\LogMeIn_Diagnostics.txt" product get Name,Vendor >nul 2>nul
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo ---Startup Programs--- >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+wmic /APPEND: "%userprofile%\Desktop\LogMeIn_Diagnostics.txt" startup get caption,command >nul 2>nul
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo ---Windows Services--- >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+wmic /APPEND: "%userprofile%\Desktop\LogMeIn_Diagnostics.txt" service get Name,State,pathname >nul 2>nul
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo ---System Drivers--- >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+@echo[ >> "%userprofile%\Desktop\LogMeIn_Diagnostics.txt"
+wmic /APPEND: "%userprofile%\Desktop\LogMeIn_Diagnostics.txt" sysdriver get Name,pathname,State >nul 2>nul
+@echo[
+@echo The diagnostic collection is finished.  The information has been stored in a file named LogMeIn_Diagnostics.txt that is located on your desktop.
+:pause
+pause
